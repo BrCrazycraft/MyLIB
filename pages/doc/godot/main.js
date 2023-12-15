5e62f9933c22bfd0ec74539151d8c6a54ad37dbe
Kaido <91611484+BrCrazycraft@users.noreply.github.com>
2023-12-15 12:29:13 -0300

    Criação de Modelo de DOC
    -Pagina de Exemplo

--- pages/doc/godot/main.js
+++ pages/doc/godot/main.js
@@ -1,3 +1,12 @@
 function updateNAV() {
-    
+    let ul = document.getElementById("LISTANAV");
+
+    let item_list1 = document.createElement("li");
+
+    let list1_a = document.createElement("a");
+    list1_a.href = "";
+    list1_a.textContent = "Element";
+
+    item_list1.appendChild(list1_a);
+    ul.appendChild(item_list1);
 }
